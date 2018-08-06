e180b7a72870f655e947df585bd02422f01e9fb0
bpina <b.pina.pinon@gmail.com>
2018-08-05 20:34:59 -0700

    FINAL- Olga, Blanca, incorporate Kevin, Andrew, Diamond

---
+++ config.py
@@ -0,0 +1,14 @@
+# Twitter API Keys
+consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
+
+consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
+
+access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+
+access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+
+# Census Keys
+census_key = "89f83991e68cbf546d83de85376d352bf66954fd"
+
+# Google Key
+gkey = "AIzaSyA9flqZFaurdCeA8Ic893G43-2u3rTGhw8"
